2e77cefe3709984f8b2dfdc8030b456a16c0064e
Gean Salasan <geansalasan@geans-MacBook-Pro.local>
2021-12-16 19:06:54 +0700

    ubah coin

--- instance.js
+++ instance.js
@@ -2,30 +2,6 @@ const c = (module.exports = {});
 
 c.symbols = [
   {
-    symbol: 'SANDUSDT',
-    exchange: 'binance_futures',
-    periods: ['15m'],
-    trade: {
-      currency_capital: 100,
-      strategies: [
-        {
-          strategy: 'noop',
-          interval: '15m',
-          options: {
-            period: '15m'
-          }
-        }
-      ]
-    },
-    watchdogs: [
-      {
-        name: 'risk_reward_ratio',
-        target_percent: 2,
-        stop_percent: 10.0
-      }
-    ]
-  },
-  {
     symbol: 'MANAUSDT',
     exchange: 'binance_futures',
     periods: ['15m'],
@@ -50,55 +26,7 @@ c.symbols = [
     ]
   },
   {
-    symbol: 'CTKUSDT',
-    exchange: 'binance_futures',
-    periods: ['15m'],
-    trade: {
-      currency_capital: 100,
-      strategies: [
-        {
-          strategy: 'noop',
-          interval: '15m',
-          options: {
-            period: '15m'
-          }
-        }
-      ]
-    },
-    watchdogs: [
-      {
-        name: 'risk_reward_ratio',
-        target_percent: 2,
-        stop_percent: 10.0
-      }
-    ]
-  },
-  {
     symbol: 'KEEPUSDT',
-    exchange: 'binance_futures',
-    periods: ['15m'],
-    trade: {
-      currency_capital: 100,
-      strategies: [
-        {
-          strategy: 'noop',
-          interval: '15m',
-          options: {
-            period: '15m'
-          }
-        }
-      ]
-    },
-    watchdogs: [
-      {
-        name: 'risk_reward_ratio',
-        target_percent: 2.5,
-        stop_percent: 10.0
-      }
-    ]
-  },
-  {
-    symbol: 'REEFUSDT',
     exchange: 'binance_futures',
     periods: ['15m'],
     trade: {
