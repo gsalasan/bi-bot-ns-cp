543a0315f0d15bb8976e6320eaaea7467121b594
Gean Salasan <geansalasan@geans-MacBook-Pro.local>
2021-12-16 14:50:14 +0700

    ubah macd

--- instance.js
+++ instance.js
@@ -9,7 +9,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -33,7 +33,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -57,7 +57,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -81,7 +81,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -105,7 +105,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -129,7 +129,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -153,7 +153,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -177,7 +177,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -201,7 +201,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
@@ -225,7 +225,7 @@ c.symbols = [
       currency_capital: 100,
       strategies: [
         {
-          strategy: 'macd',
+          strategy: 'noop',
           interval: '15m',
           options: {
             period: '15m'
